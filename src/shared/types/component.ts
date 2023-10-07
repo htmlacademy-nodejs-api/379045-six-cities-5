aef7ccede0ac5f427aa876729a42586a4ee2e4bd
Alexey Bo <b4zx720@gmail.com>
2023-10-08 00:33:39 +0300

    added DatabaseClient

--- src/shared/types/component.ts
+++ src/shared/types/component.ts
@@ -2,4 +2,5 @@ export const Component = {
   RestApp: Symbol.for('RestApp'),
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
+  DatabaseClient: Symbol.for('DatabaseClient'),
 } as const;
