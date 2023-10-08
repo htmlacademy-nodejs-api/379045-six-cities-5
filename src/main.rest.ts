a41e3c9b0cfa3d8d93aa5708bf02623d6a3a9ae6
Alexey Bo <b4zx720@gmail.com>
2023-10-08 13:21:25 +0300

    added multiple IoC containers: for User and Rest

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,17 +1,16 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApp } from './rest/index.js';
-import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestAppContainer } from './rest/index.js';
+import { createUserContainer } from './modules/user/index.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApp>(Component.RestApp).to(RestApp).inSingletonScope();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  const container = Container.merge(
+    createRestAppContainer(),
+    createUserContainer()
+  );
+
 
   const app = container.get<RestApp>(Component.RestApp);
   await app.init();
