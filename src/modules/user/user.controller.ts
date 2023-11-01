659dbb782a010d5d8a74ff63ad4a2d3c87c636ec
Alexey Bo <b4zx720@gmail.com>
2023-11-02 02:32:30 +0300

    updated user entity

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'inversify';
-import { BaseController, HttpError, UploadFileMiddleware, ValidateDtoMiddleware, ValidateObjectIdMiddleware } from '../../shared/libs/rest/index.js';
+import { BaseController, DocumentExistsMiddleware, HttpError, PrivateRouteMiddleware, UploadFileMiddleware, ValidateDtoMiddleware, ValidateObjectIdMiddleware } from '../../shared/libs/rest/index.js';
 import { Component, HttpMethod, Req } from '../../shared/types/index.js';
 import { Logger } from '../../shared/libs/logger/index.js';
 import { UserService } from './user.service.js';
@@ -7,8 +7,11 @@ import { Config, RestSchema } from '../../shared/libs/config/index.js';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { fillDTO } from '../../shared/helpers/index.js';
-import { UserRdo } from './rdo/user.rdo.js';
+import { LoggedUserRdo, UserRdo } from './rdo/user.rdo.js';
 import { CreateUserDto, LoginUserDto } from './dto/user.dto.js';
+import { AuthService } from '../auth/index.js';
+import { OfferService } from '../offer/offer.service.js';
+import { OfferRdo } from '../offer/rdo/offer.rdo.js';
 
 @injectable()
 export class UserController extends BaseController {
@@ -19,6 +22,10 @@ export class UserController extends BaseController {
     private readonly userService: UserService,
     @inject(Component.Config)
     private readonly configService: Config<RestSchema>,
+    @inject(Component.AuthService)
+    private readonly authService: AuthService,
+    @inject(Component.OfferService)
+    private readonly offerService: OfferService,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -45,6 +52,37 @@ export class UserController extends BaseController {
         new UploadFileMiddleware(this.configService.get('UPLOAD_DIR'), 'avatar'),
       ]
     });
+
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Get,
+      handler: this.checkAuthenticate,
+    });
+
+    this.addRoute({
+      path: '/favorites/',
+      method: HttpMethod.Get,
+      handler: this.index,
+      middlewares: [
+        new PrivateRouteMiddleware(),
+      ]
+    });
+
+    this.addRoute({
+      path: '/:offerId/favorites/',
+      method: HttpMethod.Patch,
+      handler: this.updateFavoriteStatus,
+      middlewares: [
+        new PrivateRouteMiddleware(),
+        new ValidateObjectIdMiddleware('offerId'),
+        new DocumentExistsMiddleware(this.offerService, { from: 'body', name: 'offerId' }),
+      ]
+    });
+  }
+
+  public async index({ tokenPayload }: Request, res: Response): Promise<void> {
+    const result = await this.userService.findFavorites(tokenPayload.id);
+    this.created(res, fillDTO(OfferRdo, result));
   }
 
   public async create({ body }: Req<CreateUserDto>, res: Response): Promise<void> {
@@ -62,25 +100,36 @@ export class UserController extends BaseController {
     this.created(res, fillDTO(UserRdo, result));
   }
 
-  public async login({ body }: Req<LoginUserDto>, _res: Response): Promise<void> {
-    const existsUser = await this.userService.findByEmail(body.email);
-
-    if (!existsUser) {
-      throw new HttpError(
-        StatusCodes.UNAUTHORIZED,
-        `User with email ${body.email} not found.`,
-        'UserController',
-      );
-    }
-
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'UserController',
-    );
+  public async login({ body }: Req<LoginUserDto>, res: Response): Promise<void> {
+    const user = await this.authService.verify(body);
+    const token = await this.authService.authenticate(user);
+    const responseData = fillDTO(LoggedUserRdo, {
+      email: user.email,
+      token,
+    });
+    this.ok(res, responseData);
   }
 
   public async uploadAvatar(req: Request, res: Response) {
     this.created(res, { filepath: req.file?.path });
+  }
+
+  public async checkAuthenticate({ tokenPayload: { email } }: Request, res: Response) {
+    const foundedUser = await this.userService.findByEmail(email);
+
+    if (!foundedUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        'Unauthorized',
+        'UserController'
+      );
+    }
+
+    this.ok(res, fillDTO(LoggedUserRdo, foundedUser));
+  }
+
+  public async updateFavoriteStatus({ body, tokenPayload }: Request<{ value?: boolean, offerId?: string }>, res: Response): Promise<void> {
+    const updatedUser = await this.userService.changeFavorites(tokenPayload.id, body.offerId as string, body.value as boolean);
+    this.noContent(res, fillDTO(UserRdo, updatedUser));
   }
 }
